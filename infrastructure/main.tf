1709816d52580bb7e21c8135ea206970175291cc
damongreen123 <damon.green@btinternet.com>
2021-02-09 17:16:47 +0000

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -5,7 +5,7 @@ provider "azurerm" {
 locals {
   vaultName = "${var.product}-${var.env}"
 }
-
+/*
 data "azurerm_subnet" "core_infra_redis_subnet" {
   name                 = "core-infra-subnet-1-${var.env}"
   virtual_network_name = "core-infra-vnet-${var.env}"
@@ -20,18 +20,18 @@ module "probate-caveats-fe-redis-cache" {
   subnetid = data.azurerm_subnet.core_infra_redis_subnet.id
   common_tags  = var.common_tags
 }
-
+*/
 data "azurerm_key_vault" "probate_key_vault" {
   name = local.vaultName
   resource_group_name = local.vaultName
 }
-
+/*
 resource "azurerm_key_vault_secret" "redis_access_key" {
   name         = "${var.component}-redis-access-key"
   value        = module.probate-caveats-fe-redis-cache.access_key
   key_vault_id = data.azurerm_key_vault.probate_key_vault.id
 }
-
+*/
 data "azurerm_key_vault" "s2s_vault" {
   name = "s2s-${var.env}"
   resource_group_name = "rpe-service-auth-provider-${var.env}"
