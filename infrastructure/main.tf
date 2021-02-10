ec608edaabe92639630dc6cf6200d3f94c8f3ae5
isha2040 <58481974+isha2040@users.noreply.github.com>
2021-02-10 14:05:27 +0000

    remove commented code

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -6,32 +6,11 @@ locals {
   aseName = "core-compute-${var.env}"
   vaultName = "${var.product}-${var.env}"
 }
-/*
-data "azurerm_subnet" "core_infra_redis_subnet" {
-  name                 = "core-infra-subnet-1-${var.env}"
-  virtual_network_name = "core-infra-vnet-${var.env}"
-  resource_group_name = "core-infra-${var.env}"
-}*/
-
-/*module "probate-caveats-fe-redis-cache" {
-  source   = "git@github.com:hmcts/cnp-module-redis?ref=master"
-  product     = "${var.product}-${var.component}-redis-cache"
-  location = "${var.location}"
-  env      = "${var.env}"
-  subnetid = "${data.azurerm_subnet.core_infra_redis_subnet.id}"
-  common_tags  = "${var.common_tags}"
-}*/
 
 data "azurerm_key_vault" "probate_key_vault" {
    name = "${local.vaultName}"
    resource_group_name = "${local.vaultName}"
 }
-
-/*resource "azurerm_key_vault_secret" "redis_access_key" {
-  name         = "${var.component}-redis-access-key"
-  value        = "${module.probate-caveats-fe-redis-cache.access_key}"
-  key_vault_id = "${data.azurerm_key_vault.probate_key_vault.id}"
-}*/
 
 data "azurerm_key_vault" "s2s_vault" {
   name = "s2s-${var.env}"
